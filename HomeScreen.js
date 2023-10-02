2189c824ad7fc935ddf34637ac0b233a4ab4f876
Heber Moran <hgmb15hjrmcmr@gmail.com>
2023-10-02 13:19:55 -0600

    Application sections implemented (template)

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,26 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { logoutUser, getUserInformation } from './firebase'; // Importa getUserInformation desde firebase.js
-import { AuthContext } from './authContext'; // Asegúrate de que el contexto de autenticación se importe desde el archivo correcto
+import { logoutUser, getUserInformation } from './firebase';
+import { AuthContext } from './authContext';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({ navigation }) => {
   const { currentUser } = useContext(AuthContext);
 
   const [email, setEmail] = useState(null);
   const [registered, setRegistered] = useState(null);
+  const [name, setName] = useState(null);
+  const [lastName, setLastName] = useState(null);
 
   useEffect(() => {
     if (currentUser) {
-      getUserInformation(currentUser) // Pasa currentUser como argumento
+      getUserInformation(currentUser)
         .then((userData) => {
           setEmail(userData.email);
           setRegistered(userData.registeredAt.toDate().toISOString().substring(0, 10));
+          setName(userData.name);
+          setLastName(userData.lastName);
         })
         .catch((error) => {
           console.error('Error fetching user information:', error);
         });
     }
-  }, [currentUser]); // Agrega currentUser como dependencia para que se vuelva a cargar cuando cambie
+  }, [currentUser]);
 
   const handleLogout = () => {
     logoutUser()
@@ -38,8 +45,45 @@ const HomeScreen = ({ navigation }) => {
       <Text>Si estás aquí, ¡tienes permiso para estarlo!</Text>
       <Text>Email: {email}</Text>
       <Text>Date of register: {registered}</Text>
+      <Text>Name: {name}</Text>
+      <Text>Last name: {lastName}</Text>
       <Button title="Logout" onPress={handleLogout} />
     </View>
+  );
+};
+
+const DonacionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Pantalla de Donación</Text>
+    </View>
+  );
+};
+
+const PerfilScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Pantalla de Perfil</Text>
+    </View>
+  );
+};
+
+const VoluntarioScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Pantalla de Voluntario</Text>
+    </View>
+  );
+};
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Principal" component={HomeScreen} />
+      <Tab.Screen name="Donacion" component={DonacionScreen} />
+      <Tab.Screen name="Perfil" component={PerfilScreen} />
+      <Tab.Screen name="Voluntario" component={VoluntarioScreen} />
+    </Tab.Navigator>
   );
 };
 
@@ -55,4 +99,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen;
+export default TabNavigator;
