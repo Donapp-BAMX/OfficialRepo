432cb4ff6e173795257f514906c77cb0beea88cd
Benjamín Gutierrez Mendoza <benjamingutierrezmendoza@Benjamins-MacBook-Pro.local>
2023-10-08 20:45:18 -0600

    completo

--- FoodRegister.js
+++ FoodRegister.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker'; // Importa DateTimePicker
 import { useNavigation } from '@react-navigation/native';
-import { addFoodToFirebase } from './firebase'; // Importa la función para agregar alimentos a Firebase
+import { addFoodToFirebase } from './firebase';
 
 const FoodRegister = () => {
   const [foodName, setFoodName] = useState('');
   const [calories, setCalories] = useState('');
+  const [expirationDate, setExpirationDate] = useState(new Date()); // Cambia a un estado de fecha
+  const [foodType, setFoodType] = useState('');
+  const [description, setDescription] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleFoodNameChange = (text) => {
@@ -16,30 +20,44 @@ const FoodRegister = () => {
     setCalories(text);
   };
 
+  const handleExpirationDateChange = (event, selectedDate) => {
+    setExpirationDate(selectedDate || expirationDate);
+  };
+
+  const handleFoodTypeChange = (type) => {
+    setFoodType(type);
+  };
+
+  const handleDescriptionChange = (text) => {
+    setDescription(text);
+  };
+
   const handleSubmit = async () => {
     try {
       setIsSubmitting(true);
 
-      // Valida que los campos no estén vacíos
-      if (!foodName || !calories) {
+      if (!foodName || !calories || !expirationDate || !foodType || !description) {
         alert('Por favor, completa todos los campos.');
         return;
       }
 
-      // Crea un objeto con los datos del alimento
       const foodData = {
         name: foodName,
         calories: parseInt(calories),
+        expirationDate,
+        foodType,
+        description,
       };
 
-      // Agrega el alimento a Firebase
       const foodId = await addFoodToFirebase(foodData);
 
       alert(`Alimento registrado con éxito. ID: ${foodId}`);
 
-      // Limpia los campos del formulario
       setFoodName('');
       setCalories('');
+      setExpirationDate(new Date());
+      setFoodType('');
+      setDescription('');
     } catch (error) {
       alert('Error al registrar el alimento: ' + error.message);
     } finally {
@@ -52,6 +70,13 @@ const FoodRegister = () => {
   const handleBackPress = () => {
     navigation.goBack();
   };
+
+  const foodTypes = [
+    { label: 'Fruta/Verdura', value: 'fruta_verdura' },
+    { label: 'Abrefácil', value: 'abrefacil' },
+    { label: 'Lata', value: 'lata' },
+    { label: 'Otros', value: 'otros' },
+  ];
 
   return (
     <View style={styles.container}>
@@ -68,6 +93,36 @@ const FoodRegister = () => {
         placeholder="Calorías"
         keyboardType="numeric"
         style={styles.input}
+      />
+      <View style={styles.datePickerContainer}>
+        <Text>Fecha de caducidad: </Text>
+        <DateTimePicker
+          value={expirationDate}
+          mode="date"
+          display="default"
+          onChange={handleExpirationDateChange}
+        />
+      </View>
+      <View style={styles.foodTypeButtons}>
+        {foodTypes.map((type) => (
+          <TouchableOpacity
+            key={type.value}
+            style={[
+              styles.foodTypeButton,
+              foodType === type.value ? styles.selectedFoodTypeButton : {},
+            ]}
+            onPress={() => handleFoodTypeChange(type.value)}
+          >
+            <Text>{type.label}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <TextInput
+        value={description}
+        onChangeText={handleDescriptionChange}
+        placeholder="Descripción del alimento"
+        style={styles.input}
+        multiline
       />
       <Button title="Registrar" onPress={handleSubmit} disabled={isSubmitting} />
       <Button title="Volver" onPress={handleBackPress} />
@@ -92,6 +147,27 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     marginBottom: 20,
     padding: 10,
+  },
+  datePickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  foodTypeButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginBottom: 20,
+  },
+  foodTypeButton: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    alignItems: 'center',
+  },
+  selectedFoodTypeButton: {
+    backgroundColor: 'lightblue',
   },
 });
 
