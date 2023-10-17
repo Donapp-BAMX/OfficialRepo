8ed87dc2f7ab78217bfa9b95e7e93014d1f11098
cexuar <129445123+cexuar@users.noreply.github.com>
2023-10-17 16:01:36 -0600

    FrontDos (#18)
    Co-authored-by: cexuar <104097162+cesarcc0110@users.noreply.github.com>

--- DonationScreen.js
+++ DonationScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Linking } from 'react-native';
 
@@ -17,40 +17,81 @@ function DonationView() {
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.buttonContainer} onPress={handleDonateMoney}>
-        <Text style={styles.buttonText}>Donar Dinero</Text>
-        <Image source={require('./img/creditCard.png')} style={styles.imageCreditCard} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonContainer} onPress={handleTDonateFood}>
-        <Text style={styles.buttonText}>Donar Comida</Text>
-        <Image source={require('./img/food.png')} style={styles.imageFood} />
-      </TouchableOpacity>
+    <View style={styles.fullScreen}>
+      <ImageBackground source={require('./assets/nino.jpg')} style={styles.backgroundImage} />
+      <View style={styles.container}>
+        
+        {/* Texto principal */}
+        <View style={styles.headingContainer}>
+          <Text style={styles.mainHeading}>¿Qué desea donar?</Text>
+        </View>
+
+        {/* Botones */}
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.customButton} onPress={handleDonateMoney}>
+            <Text style={styles.buttonText}>Donar Dinero</Text>
+            <Image source={require('./img/creditCard.png')} style={styles.imageCreditCard} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.customButton} onPress={handleTDonateFood}>
+            <Text style={styles.buttonText}>Donar Comida</Text>
+            <Image source={require('./img/food.png')} style={styles.imageFood} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+  },
+  headingContainer: {
+    marginBottom: 10, 
+    marginTop: 80,
+  },
+  buttonsContainer: {
     alignItems: 'center',
   },
-  buttonContainer: {
+  mainHeading: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    color: "#ffffff",
+  },
+  customButton: {
+    backgroundColor: '#FFD700',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+    marginBottom: 10,
+    marginTop: 10,
     width: 250,
     height: 60,
-    borderWidth: 1,
-    borderColor: 'black',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  buttonText: {
-    textAlign: 'left',
-    flex: 1,
-    fontSize: 18,
   },
   imageCreditCard: {
     width: 70,
@@ -60,6 +101,11 @@ const styles = StyleSheet.create({
     width: 55,
     height: 55,
     marginRight: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
   },
 });
 
