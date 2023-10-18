6b67bdc501b640aff85ce9ddc03d97f973cacf3a
Benjamín Gutierrez Mendoza <benjamingutierrezmendoza@Benjamins-MacBook-Pro.local>
2023-10-17 18:50:00 -0600

    fornt y mejoramiento de anuncios

--- AdDetails.js
+++ AdDetails.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
-const AdDetails = ({ route }) => {
+const AdDetails = ({ route, navigation }) => {
   const { title, description } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
+      <Card containerStyle={styles.card}>
+        <Card.Title style={styles.title}>{title}</Card.Title>
+        <Card.Divider />
+        <Text style={styles.description}>{description}</Text>
+        <Button
+          title="Regresar"
+          buttonStyle={styles.button}
+          titleStyle={styles.buttonTitle}
+          //onPress={() => navigation.navigate('HomeScreen.js')}
+          //quiero que me regrese a homsecreen.js
+            onPress={() => navigation.goBack()}
+
+        />
+      </Card>
     </View>
   );
 };
@@ -18,13 +31,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    width: '80%',
+    borderRadius: 10,
+  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: 'black',
   },
   description: {
     fontSize: 18,
     marginTop: 10,
+    color: 'black',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'yellow',
+    marginTop: 10, // Agregamos espacio entre el texto y el botón
+  },
+  buttonTitle: {
+    color: 'red',
   },
 });
 
