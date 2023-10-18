ecf740909a8e92ea82f07328e8e48dd3c6e61c0d
Heber Moran <hgmb15hjrmcmr@gmail.com>
2023-10-18 15:08:50 -0600

    Optimización al obtener las tareas para los voluntarios

--- volunteerTasks.js
+++ volunteerTasks.js
@@ -1,10 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, ActivityIndicator, TouchableOpacity, Modal, TextInput, FlatList } from 'react-native';
-import { updateTask, getUserInformation, saveTasks, getTasks } from './firebase'; // Importa getAnuncios
-import { AuthContext } from './authContext';
-import { useNavigation } from '@react-navigation/native'; // Importa useNavigation desde React Navigation
-import TaskDetail from './taskDetail';
-import { Card, Input, Button as RNEButton } from 'react-native-elements'; // Importa el componente Card, Input y Button de react-native-elements
+import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity, Modal, TextInput, FlatList } from 'react-native';
+import { getUserInformation, saveTasks, getTasks } from './firebase';
+import { useNavigation } from '@react-navigation/native';
+import { Card, Button as RNEButton } from 'react-native-elements';
 
 const VolunteerTasks = ({ currentUser }) => {
   const navigation = useNavigation();
@@ -14,26 +12,39 @@ const VolunteerTasks = ({ currentUser }) => {
   const [description, setDescription] = useState('');
   const [volunteers, setVolunteers] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  if (currentUser) {
-    getUserInformation(currentUser)
-      .then((userData) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await getUserInformation(currentUser);
         setHasIdTrabajo(userData.idTrabajo !== undefined);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error('Error fetching user information:', error);
-      });
+      }
 
-    getTasks()
-      .then((anunciosData) => {
+      try {
+        const anunciosData = await getTasks();
         setTasks(anunciosData);
-      })
-      .catch((error) => {
-        console.error('Error fetching announcements:', error);
-      });
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (currentUser && loading) {
+      fetchData();
+    }
+  }, [currentUser, loading]);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#FFD700" />
+      </View>
+    );
   }
-}, [currentUser]);
 
   const handleCreateTask = () => {
     setShowCreateTaskForm(true);
@@ -48,21 +59,15 @@ useEffect(() => {
       console.error('Título, descripción y número de voluntarios son obligatorios.');
       return;
     }
-  
+
     try {
       const taskId = await saveTasks(title, description, volunteers);
       setShowCreateTaskForm(false);
-  
-      // Obtén la lista actual de tareas
-      const currentTasks = [...tasks];
-  
-      // Agrega la nueva tarea al principio de la lista
-      currentTasks.unshift({ id: taskId, title, description, volunteers });
-  
-      // Actualiza el estado con la nueva lista de tareas
+
+      const currentTasks = [{ id: taskId, title, description, volunteers }, ...tasks];
+
       setTasks(currentTasks);
-  
-      // Restablece los valores de title, description y volunteers
+
       setTitle('');
       setDescription('');
       setVolunteers('');
@@ -71,73 +76,75 @@ useEffect(() => {
     }
   };
 
-return (
+  return (
     <View style={styles.container}>
-        {hasIdTrabajo && (
-            <View style={styles.createAdButton}>
-                <TouchableOpacity onPress={handleCreateTask}>
-                    <Text style={styles.createAdButtonText}>+</Text>
-                </TouchableOpacity>
-            </View>
-        )}
+      {hasIdTrabajo && (
+        <View style={styles.createAdButton}>
+          <TouchableOpacity onPress={handleCreateTask}>
+            <Text style={styles.createAdButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      )}
 
-        <Modal visible={showCreateTaskForm} animationType="slide">
-            <View style={styles.modalContainer}>
-            <Text>Título de la tarea:</Text>
-                <TextInput
-                placeholder="Título"
-                value={title}
-                onChangeText={(text) => setTitle(text)}
-                style={styles.input}
-            />
-            <Text>Descripción de la tarea:</Text>
-            <TextInput
-                placeholder="Descripción"
-                value={description}
-                onChangeText={(text) => setDescription(text)}
-                style={styles.input}
-            />
-            <Text>Voluntarios Requeridos:</Text>
-            <TextInput
-                placeholder="Voluntarios"
-                value={volunteers}
-                onChangeText={(text) => setVolunteers(text)}
-                style={styles.input}
-            />
-            <RNEButton title="Guardar Tarea"             
+      <Modal visible={showCreateTaskForm} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text>Título de la tarea:</Text>
+          <TextInput
+            placeholder="Título"
+            value={title}
+            onChangeText={setTitle}
+            style={styles.input}
+          />
+          <Text>Descripción de la tarea:</Text>
+          <TextInput
+            placeholder="Descripción"
+            value={description}
+            onChangeText={setDescription}
+            style={styles.input}
+          />
+          <Text>Voluntarios Requeridos:</Text>
+          <TextInput
+            placeholder="Voluntarios"
+            value={volunteers}
+            onChangeText={setVolunteers}
+            style={styles.input}
+          />
+          <RNEButton
+            title="Guardar Tarea"
             buttonStyle={styles.saveButton}
             titleStyle={styles.saveButtonText}
-            onPress={handleSaveTask} 
-            />
-            <RNEButton title="Cancelar" 
+            onPress={handleSaveTask}
+          />
+          <RNEButton
+            title="Cancelar"
             buttonStyle={styles.cancelButton}
             titleStyle={styles.cancelButtonText}
-            onPress={handleCloseCreateTaskForm} 
-            />
-            </View>
-        </Modal>
+            onPress={handleCloseCreateTaskForm}
+          />
+        </View>
+      </Modal>
 
-        <FlatList
-            data={tasks}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <Card containerStyle={styles.anuncioItem}>
-                <TouchableOpacity
-                  style={styles.taskItem}
-                  onPress={() => {
-                      navigation.navigate('TaskDetail', { anuncio: item, });
-                  }}
-                  >
-                  <Text style={styles.anuncioTitle}>{item.title}</Text>
-                  <Text style={styles.anuncioDescription}>
-                      {"Requeridos: " + item.volunteers + '\n' + "Al momento: " + item.currentVol + '\n'}
-                      {item.description.length > 50 ? item.description.slice(0, 50) + '...' : item.description}
-                  </Text>
-                </TouchableOpacity>
-              </Card>
-            )}
-        />
-
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Card containerStyle={styles.anuncioItem}>
+            <TouchableOpacity
+              style={styles.taskItem}
+              onPress={() => {
+                navigation.navigate('TaskDetail', { anuncio: item });
+              }}
+            >
+              <Text style={styles.anuncioTitle}>{item.title}</Text>
+              <Text style={styles.anuncioDescription}>
+                {`Requeridos: ${item.volunteers}\nAl momento: ${item.currentVol}\n${
+                  item.description.length > 50 ? item.description.slice(0, 50) + '...' : item.description
+                }`}
+              </Text>
+            </TouchableOpacity>
+          </Card>
+        )}
+      />
     </View>
   );
 };
@@ -147,11 +154,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  background: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: 'linear-gradient(to bottom, red, yellow)', // Degradado de rojo a amarillo
   },
   createAdButton: {
     position: 'absolute',
@@ -163,7 +165,7 @@ const styles = StyleSheet.create({
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
-  }, 
+  },
   createAdButtonText: {
     color: 'white',
     fontSize: 30,
@@ -173,15 +175,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  inputContainer: {
+  input: {
     width: '80%',
     marginBottom: 10,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 4,
   },
-  inputField: {
-    width: '100%', // Centra el campo de entrada
+  saveButton: {
+    backgroundColor: '#FFD700',
+  },
+  saveButtonText: {
+    color: 'white',
+  },
+  cancelButton: {
+    backgroundColor: 'gray',
+    marginTop: 10,
+  },
+  cancelButtonText: {
+    color: 'white',
   },
   anuncioItem: {
     padding: 10,
@@ -197,5 +209,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
-  
+
 export default VolunteerTasks;
